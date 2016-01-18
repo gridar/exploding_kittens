fd3e5a3ea04d288807b5d8e9e7931f0cd96c5092
Julien Girard <julien@tymate.com>
2016-01-18 10:42:40 +0100

    refacto nope cards

--- step1_exploding_kittens/src/step1_exploding_kittens/Model/Engine.java
+++ step1_exploding_kittens/src/step1_exploding_kittens/Model/Engine.java
@@ -58,24 +58,20 @@ public class Engine {
     
     public void playTurn(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("player's turn : "+currentPlayer.name);
+        System.out.println("player's turn : " + currentPlayer.name);
         System.out.println("Cards");
         for(int i=0; i < currentPlayer.cards.size(); i++){
-            System.out.println(i+"- "+currentPlayer.cards.get(i).name+" _ effect : "+currentPlayer.cards.get(i).effect);
+            System.out.println(i + "- " + currentPlayer.cards.get(i).name + " _ effect : " + currentPlayer.cards.get(i).effect);
         }
         System.out.println("Play card ? card's number ?");  
         String card_play_number = scanner.nextLine();
-        Card card_play=currentPlayer.cards.get(Integer.parseInt(card_play_number));
+        Card card_play = currentPlayer.cards.get(Integer.parseInt(card_play_number));
         //effect card
+        System.println(card_play.name);
         
-        int counter_card = counterCardPlayers(0);
-        if(counter_card%2 == 0){
-            //effect counter card
-            //battle counter card with currentPlayer and currentCounterPlayer
-        }else{
-            //counter card
+        if(!blocked()){
+            card_play.play();
         }
-        
     }
     
     public Player nextPlayer(){
@@ -90,63 +86,42 @@ public class Engine {
         }
         return currentPlayer;
     }
-    
-    public int counterCardPlayers(int nCounter){
-        Card card_tmp = new Card();
-        
-        do{
-            currentCounterPlayer= nextCounterPlayer();
-            if(currentCounterPlayer==null){
-                //error
-            }
-            
-           card_tmp = counterCard(currentCounterPlayer); 
-            if(card_tmp!=null){
-                if(card_tmp.name.equals("Nope")){
-                    counterCardPlayers(nCounter++);
-                }
-            } 
-        }while(!currentPlayer.name.equals(currentCounterPlayer.name));
-        
-        return nCounter;
+
+    public boolean blocked(){
+        return this.blocked(this.getCurrentPlayer());
     }
-    public Card counterCard(Player player){
-        Scanner scanner = new Scanner(System.in);        
-        String card_play_number="";
-        Card card_play=new Card();
-        System.out.println(currentCounterPlayer.name+" counter play with a Nope card ?");
-        for(int i=0; i < player.cards.size(); i++){
-            System.out.println(i+"- "+player.cards.get(i).name+" _ effect : "+player.cards.get(i).effect);
-        }
-        System.out.println("Play card ? card's number or n ?");  
-        card_play_number = scanner.nextLine();
-        if(card_play_number.equals("n")){
-            return null;
-        }else{
-            card_play= player.cards.get(Integer.parseInt(card_play_number));
-            if(card_play.name.equals("Nope")){
-                return card_play;
-            }else{
-                System.out.println("Wrong card play");
-                counterCard(player);
+    public boolean blocked(Player player){
+
+        boolean blocked = false;
+        int index = getPlayerIndex(player);
+        int nbPlayer = this.getNbPlayers();
+
+        // loop on all players starting with current player
+        for(int i = index; i < index + nbPlayer ; i++) {
+            tmp_index = i % nbPlayer;
+            Player tmp_tmp_player = this.tmp_players.get(tmp_index);
+            if(tmp_player.haveBlockCard() && tmp_player.selectBlockCard()){
+                if(!blocked(tmp_player)){
+                    blocked = true;
+                    break;
+                }
             }
-            
+        
         }
-        return null;
+        return blocked;
     }
 
-    private Player nextCounterPlayer() {
-        for (int i = 0; i < players.size(); i++) {
-            if(players.get(i).equals(currentCounterPlayer.name)){
-                if(i==players.size()-1){
-                    currentCounterPlayer= players.get(0);
-                }else{
-                     currentCounterPlayer= players.get(i+1);
-                }
-                return currentCounterPlayer;
-            }
+    public boolean selectBlockCard(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Play Nope card ? y or n ?");
+        switch (scanner.nextLine()) {
+            case "y": return true;
+                break;
+            case "n": return false;
+                break;
+            default: return selectBlockCard();
+                break;
         }
-        return null;
     }
 
     Object getCurrentPlayer() {
